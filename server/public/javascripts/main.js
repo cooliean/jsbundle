62ed7851ab6b527d7c938e081632c510783e1b87
lh_wang <lh_wang@Ctrip.com>
2015-09-14 14:05:54 +0800

    页面切换效果

--- server/public/javascripts/main.js
+++ server/public/javascripts/main.js
@@ -9,11 +9,45 @@
       password: password
     }, function(data){
       if(data.status){
-        window.location.href = '/index';
+        global.location.href = '/index';
       }else{
         alert('失败');
       }
     });
   });
+
+
+  /*修改底部按钮的样式*/
+  var url = global.location.href;
+  if(url.indexOf('index') > 0){
+    addActive('index');
+  }
+  if(url.indexOf('upload') > 0){
+    addActive('upload');
+  }
+  if(url.indexOf('about') > 0){
+    addActive('about');
+  }
+
+
+  /*修改底部active*/
+  function addActive(name){
+    $('#index_menu i').removeClass('icon_active');
+    $('#index_menu .icon_text').removeClass('icon_active');
+
+    $('#upload_menu i').removeClass('icon_active');
+    $('#upload_menu .icon_text').removeClass('icon_active');
+
+    $('#about_menu i').removeClass('icon_active');
+    $('#about_menu .icon_text').removeClass('icon_active');
+
+    console.log(name);
+    $('#' + name + '_menu i').addClass('icon_active');
+    $('#' + name + '_menu .icon_text').addClass('icon_active');
+  }
+
+
+
+
 
 })($, window,undefined);
